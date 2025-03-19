96684deddbefd0a7e8bad8516e202045a139d2e3
hs-1971209-sangyunlee <son5165@naver.com>
2025-03-19 09:27:17 +0900

    add quest

--- app/src/main/java/com/example/dailyquest/fragment/AddFragment.kt
+++ app/src/main/java/com/example/dailyquest/fragment/AddFragment.kt
@@ -3,21 +3,27 @@ package com.example.dailyquest.fragment
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.dailyquest.R
 import com.example.dailyquest.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import io.grpc.Deadline
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
+import java.util.UUID
 
 class AddFragment : Fragment() {
     val fragmentTitle: String get() = "Add"
     private lateinit var deadline: TextView
+    private var userId: String? = null
     private var selectedCalendar: Calendar = Calendar.getInstance()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,16 +31,22 @@ class AddFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_add, container, false) // XML 연결
 
+        userId = FirebaseAuth.getInstance().currentUser?.uid // ✅ 현재 로그인한 사용자 UID 가져오기
+
         deadline = view.findViewById<TextView>(R.id.deadlineTextView)
 
         //초기값: 현재시각 +24시간
         selectedCalendar.add(Calendar.DAY_OF_YEAR, 1)
         updateDeadlineText()
 
-        deadline.setOnClickListener{
+        deadline.setOnClickListener {
             showDateTimePicker()
         }
 
+        val addButton = view.findViewById<Button>(R.id.addQuestButton)
+        addButton.setOnClickListener {
+            savaQuestToFireStore()
+        }
         return view
     }
 
@@ -72,5 +84,35 @@ class AddFragment : Fragment() {
     private fun updateDeadlineText() {
         val format = SimpleDateFormat("yyyy/MM/dd HH:mm", Locale.getDefault())
         deadline.text = format.format(selectedCalendar.time)
+    }
+
+    private fun savaQuestToFireStore() {
+        val title = "테스트"
+        val category = "일일퀘스트"
+        val xp = 500
+
+        // ✅ Firestore에 저장할 데이터 객체
+        val questData = hashMapOf(
+            "category" to category,
+            "title" to title,
+            "xp" to xp,
+            "period" to "~"+deadline.text,
+            "completed" to false
+        )
+
+        val uniqueId = UUID.randomUUID().toString()
+        val db = FirebaseFirestore.getInstance()
+        // ✅ Firestore 저장 (uid > 사용자id > 일일퀘스트 > title)
+        userId?.let {
+            db.collection("uid").document(it)
+                .collection(category).document(uniqueId)
+                .set(questData)
+                .addOnSuccessListener {
+                    Log.d("Firestore", "퀘스트 저장 성공!")
+                }
+                .addOnFailureListener { e ->
+                    Log.e("Firestore", "퀘스트 저장 실패", e)
+                }
+        }
     }
 }
